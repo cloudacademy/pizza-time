334499d3e94857a21ff95694b1f3e092e92b8797
Eric Magalhães <ericovis@gmail.com>
2016-07-29 19:33:09 -0300

    added custom scripts

--- scripts/send_custom_metrics.sh
+++ scripts/send_custom_metrics.sh
@@ -3,4 +3,4 @@
 ADDRESS="pizza.clouda.rocks"
 CURL=$(curl -w %{time_total} -o /dev/null -s $ADDRESS)
 
-aws cloudwatch put-metric-data --metric-name LoadTime --namespace "Pizza time" --value $CURL --unit 'Seconds' --dimensions Env=Prod --region eu-central-1
+aws cloudwatch put-metric-data --metric-name LoadTime --namespace "Pizza time" --value $CURL --unit 'Seconds' --dimensions Env=Prod --region us-west-2
