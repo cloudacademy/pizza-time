b4ba748120e5763687d2397e417a62c8c6ca4c52
Eric Magalhães <ericovis@gmail.com>
2016-07-29 20:31:07 -0300

    fixed scripts

--- scripts/install_custom_monitoring.sh
+++ scripts/install_custom_monitoring.sh
@@ -7,4 +7,5 @@ unzip CloudWatchMonitoringScripts-1.2.1.zip
 rm CloudWatchMonitoringScripts-1.2.1.zip
 cd aws-scripts-mon
 echo "*/5 * * * * /monitoring/aws-scripts-mon/mon-put-instance-data.pl --mem-util --swap-util --disk-space-util --disk-space-used --disk-path=/ --from-cron" > crontab.txt
+sleep 5
 crontab crontab.txt
