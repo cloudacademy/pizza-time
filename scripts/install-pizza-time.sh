ee3814485cacf929f356d24536b9ae1390e76afd
Eric Magalhães <ericovis@gmail.com>
2016-08-05 00:23:13 -0300

    install pizza-time script

---
+++ scripts/install-pizza-time.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export DATABASE_NAME=
+export DATABASE_USER=
+export DATABASE_HOST=
+export DATABASE_PORT=3306
+export DATABASE_PASSWORD=
+yum update -y
+yum groupinstall -y "Development Tools"
+yum install -y git mysql-devel mysql-libs kernel-devel
+git clone https://github.com/cloudacademy/pizza-time
+cd pizza-time
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py runserver 0.0.0.0:80
